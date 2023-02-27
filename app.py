03832739111dde15558f6465adf94bbe8e11beba
Maikel Braas <maikelbraas@gmail.com>
2023-02-27 11:48:43 +0000

    first page

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+TASKS = [
+  {
+    "id" : 1,
+    "title" : "Fill in the logboek: 1",
+    "content" : "The logboek hasn't been filled yet, fill it."
+  },
+  {
+    "id" : 2,
+    "title" : "Fill in the logboek: 2",
+    "content" : "The logboek hasn't been filled yet, fill it."
+  },
+  {
+    "id" : 3,
+    "title" : "Fill in the logboek: 3",
+    "content" : "The logboek hasn't been filled yet, fill it."
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return "<p>Hello Page!</p>"
+  return render_template("home.html", tasks=TASKS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
